22ace0e3c0f3fd17638c2062f5f9a5236a9f36d2
hackerliang <1254548379@qq.com>
2019-11-03 21:01:02 +0800

    Classify files

--- WordCount/reducer.py
+++ WordCount/reducer.py
@@ -26,6 +26,3 @@ word_dict = sorted(word_dict.items())
 
 for k, v in word_dict:
     print('%s\t%s' % (k, v))
-
-#hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.2.0.jar -file mapper.py -mapper "python mapper.py" -file reducer.py -reducer "python reducer.py" -input /user/uic/py_wc_input/* -output /user/uic/py_wc_output/
-#mr-jobhistory-daemon.sh start historyserver
